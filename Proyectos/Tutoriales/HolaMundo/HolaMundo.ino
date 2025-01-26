a60f1cc0a13ba73c0127b4ec9c9e999f8ac0f955
EstebanG <estegar007@gmail.com>
2025-01-26 15:47:36 -0500

    update HolaMundo.ino Agregar comentarios

--- Proyectos/Tutoriales/HolaMundo/HolaMundo.ino
+++ Proyectos/Tutoriales/HolaMundo/HolaMundo.ino
@@ -1,3 +1,11 @@
+/* 
+  Proyecto: Hola Mundo
+  Descripción: Se hace uso del puerto serie para mostrar un mensaje desde el ESP32. 
+  Autor: David Esteban Garnica - Ingeniería Electrónica
+  Fecha de creación: 26/01/2024
+  Última modificación: 26/01/2024
+*/
+
 void setup() {
   Serial.begin(115200);     // Inicia el puerto serial a 115200 baudios
 }
